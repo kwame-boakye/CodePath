a0b4cd9316563ebfc4f2a0f4ccadfc3de3985338
kwame-boakye <boakyekelvin306@gmail.com>
2025-02-20 21:28:43 -0600

    Work on standard version 2

---
+++ Unit-1/Session-1/Standard-V2/batman.py
@@ -0,0 +1,9 @@
+# Write a function batman() that prints the string
+# "I am vengeance. I am the night. I am Batman!".
+
+
+def batman():
+    return "I am vengeance. I am the night. I am Batman!"
+
+
+print(batman())
